9e4fe69770017bfd6ef9c6ed9aa2791d7447b3bb
Craft <khadija@craftinteractive.ae>
2024-01-09 11:21:09 +0530

    add: employment contract and document request form

---
+++ craft_hr/craft_hr/doctype/document_request_form/document_request_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Craftinteractive and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Document Request Form', {
+	// refresh: function(frm) {
+
+	// }
+});
