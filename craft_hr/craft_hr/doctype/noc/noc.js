f85811273ebfe401dadffd3a9a09faa408b27b0d
Craft <khadija@craftinteractive.ae>
2024-01-10 11:23:58 +0530

    fix: letters

--- craft_hr/craft_hr/doctype/noc/noc.js
+++ craft_hr/craft_hr/doctype/noc/noc.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('NOC', {
-	// refresh: function(frm) {
+	letter_template: function(frm){
+		if (frm.doc.letter_template){
+			frappe.call({
+				method: 'craft_hr.craft_hr.doctype.letter_template.letter_template.get_letter_details',
+				args : {
+					template : frm.doc.letter_template
+				},
+				callback: function(r){
+					if(r.message){
+						let message_body = r.message;
+						frm.set_value("subject", message_body[0].subject);
+						frm.set_value("content", message_body[0].content);
+						frm.refresh();
+					}
+				}
 
-	// }
+			});
+		}
+	},
 });
