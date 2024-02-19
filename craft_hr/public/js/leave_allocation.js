0d4e694754c2a28394156975a7c0f42616f64770
Khadija Craft <khadija@craftinteractive.ae>
2024-02-19 14:06:56 +0400

    settings for distribution based leave

--- craft_hr/public/js/leave_allocation.js
+++ craft_hr/public/js/leave_allocation.js
@@ -1,5 +1,13 @@
 
 frappe.ui.form.on("Leave Allocation",{
+    refresh: function(frm){
+        frappe.db.get_single_value("Craft HR Settings", "leave_allocation_based_on_leave_distribution_template").then((value) => {
+            frm.toggle_display("custom_is_earned_leave", value)
+            if(frm.doc.docstatus != 1 && frm.doc.custom_is_earned_leave && !value){
+                frm.set_value("custom_is_earned_leave",0)
+            }
+        });
+    },
     onload: function(frm){
         if(frm.doc.custom_status=="Ongoing" && frm.doc.docstatus){
             frm.add_custom_button(__('Close'), function(){
