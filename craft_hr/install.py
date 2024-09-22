e38d5dc9d35580d0272d37089dee88fc4e678411
Midhun George <support@keystoneuae.com>
2024-09-22 15:25:25 +0400

    update the leave distribution

--- craft_hr/install.py
+++ craft_hr/install.py
@@ -7,11 +7,11 @@ def after_install():
         "maximum_leaves":0,
         "doctype":"Leave Distribution Template",
         "leave_distribution":[
-        {"idx":1,"start":1,"end":5,"monthly_allocation":0},
-        {"idx":2,"start":6,"end":6,"monthly_allocation":12},
-        {"idx":3,"start":7,"end":11,"monthly_allocation":2},
-        {"idx":4,"start":12,"end":12,"monthly_allocation":8},
-        {"idx":5,"start":13,"end":0,"monthly_allocation":2.5}]
+        {"idx":1,"start":1,"end":5,"value_allocation":0},
+        {"idx":2,"start":6,"end":6,"value_allocation":12},
+        {"idx":3,"start":7,"end":11,"value_allocation":2},
+        {"idx":4,"start":12,"end":12,"value_allocation":8},
+        {"idx":5,"start":13,"end":0,"value_allocation":2.5}]
     }
 
     if not frappe.db.exists("Leave Distribution Template", document_details["title"]):
